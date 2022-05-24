eee138ed1918aaf0ed31cac79d6ce1851497a692
Hao Ruan <righthandss@gmail.com>
2022-05-24 20:15:32 +0800

    [WIP] add part of prefilterplugin framework

--- sample-scheduler/framework.go
+++ sample-scheduler/framework.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"context"
+	"fmt"
+
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+	v1 "k8s.io/kubernetes/staging/src/k8s.io/api/core/v1"
 )
 
 type Framework interface {
@@ -12,8 +16,31 @@ type Framework interface {
 	HasFilterPlugins() bool
 }
 
+// Plugin is the parent type for all the scheduling framework plugins.
+type Plugin interface {
+	Name() string
+}
+
+// PreFilterPlugin is an interface that must be implemented by "PreFilter" plugins.
+// These plugins are called at the beginning of the scheduling cycle.
+type PreFilterPlugin interface {
+	Plugin
+	// PreFilter is called at the beginning of the scheduling cycle. All PreFilter
+	// plugins must return success or the pod will be rejected. PreFilter could optionally
+	// return a PreFilterResult to influence which nodes to evaluate downstream. This is useful
+	// for cases where it is possible to determine the subset of nodes to process in O(1) time.
+	PreFilter(ctx context.Context, state *CycleState, p *v1.Pod) (*PreFilterResult, *Status)
+	// PreFilterExtensions returns a PreFilterExtensions interface if the plugin implements one,
+	// or nil if it does not. A Pre-filter plugin can provide extensions to incrementally
+	// modify its pre-processed info. The framework guarantees that the extensions
+	// AddPod/RemovePod will only be called after PreFilter, possibly on a cloned
+	// CycleState, and may call those functions more than once before calling
+	// Filter again on a specific node.
+	PreFilterExtensions() PreFilterExtensions
+}
+
 type frameworkImpl struct {
-	// preFilterPlugins []PreFilterPlugin
+	preFilterPlugins []PreFilterPlugin
 }
 
 // PreFilterResult wraps needed info for scheduler framework to act upon PreFilter phase.
@@ -21,6 +48,43 @@ type PreFilterResult struct {
 	// The set of nodes that should be considered downstream; if nil then
 	// all nodes are eligible.
 	NodeNames map[string]struct{}
+}
+
+func (p *PreFilterResult) Merge(in *PreFilterResult) *PreFilterResult {
+	if p.AllNodes() && in.AllNodes() {
+		return nil
+	}
+
+	r := PreFilterResult{}
+	if p.AllNodes() {
+		for k := range in.NodeNames {
+			r.NodeNames[k] = struct{}{}
+		}
+		return &r
+	}
+	if in.AllNodes() {
+		for k := range p.NodeNames {
+			r.NodeNames[k] = struct{}{}
+		}
+		return &r
+	}
+
+	// Intersection
+	var walk, other map[string]struct{}
+	if len(p.NodeNames) < len(in.NodeNames) {
+		walk = p.NodeNames
+		other = in.NodeNames
+	} else {
+		walk = in.NodeNames
+		other = p.NodeNames
+	}
+	for key := range walk {
+		if _, f := other[key]; f {
+			r.NodeNames[key] = struct{}{}
+		}
+	}
+
+	return &r
 }
 
 func (p *PreFilterResult) AllNodes() bool {
@@ -37,30 +101,30 @@ func (f *frameworkImpl) RunPreFilterPlugins(ctx context.Context, pod *Pod) (*Pre
 	// 	metrics.FrameworkExtensionPointDuration.WithLabelValues(preFilter, status.Code().String(), f.profileName).Observe(metrics.SinceInSeconds(startTime))
 	// }()
 	var result *PreFilterResult
-	// var pluginsWithNodes []string
-	// for _, pl := range f.preFilterPlugins {
-	// 	r, s := f.runPreFilterPlugin(ctx, pl, state, pod)
-	// 	if !s.IsSuccess() {
-	// 		s.SetFailedPlugin(pl.Name())
-	// 		if s.IsUnschedulable() {
-	// 			return nil, s
-	// 		}
-	// 		return nil, framework.AsStatus(fmt.Errorf("running PreFilter plugin %q: %w", pl.Name(), status.AsError())).WithFailedPlugin(pl.Name())
-	// 	}
-	// 	if !r.AllNodes() {
-	// 		pluginsWithNodes = append(pluginsWithNodes, pl.Name())
-	// 	}
-	// 	result = result.Merge(r)
-	// 	if !result.AllNodes() && len(result.NodeNames) == 0 {
-	// 		msg := fmt.Sprintf("node(s) didn't satisfy plugin(s) %v simultaneously", pluginsWithNodes)
-	// 		if len(pluginsWithNodes) == 1 {
-	// 			msg = fmt.Sprintf("node(s) didn't satisfy plugin %v", pluginsWithNodes[0])
-	// 		}
-	// 		return nil, framework.NewStatus(framework.Unschedulable, msg)
-	// 	}
+	var pluginsWithNodes []string
+	for _, pl := range f.preFilterPlugins {
+		r, err := f.runPreFilterPlugin(ctx, pl, pod)
+		if err != nil {
+			return nil, err
+		}
+		if !r.AllNodes() {
+			pluginsWithNodes = append(pluginsWithNodes, pl.Name())
+		}
+		result = result.Merge(r)
+		if !result.AllNodes() && len(result.NodeNames) == 0 {
+			msg := fmt.Sprintf("node(s) didn't satisfy plugin(s) %v simultaneously", pluginsWithNodes)
+			if len(pluginsWithNodes) == 1 {
+				msg = fmt.Sprintf("node(s) didn't satisfy plugin %v", pluginsWithNodes[0])
+			}
+			return nil, framework.NewStatus(framework.Unschedulable, msg)
+		}
 
-	// }
+	}
 	return result, nil
+}
+
+func (f *frameworkImpl) runPreFilterPlugin(ctx context.Context, pl PreFilterPlugin, pod *Pod) (*PreFilterResult, error) {
+	return nil, nil
 }
 
 func (f *frameworkImpl) RunReservePluginsReserve(ctx context.Context, pod *Pod, nodeName string) error {
